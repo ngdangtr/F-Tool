b45d3f99cb5fd3aeb9198bb20f9b3df6573d0bbc
Nguyễn Đăng Trình <110520177+ngdangtr@users.noreply.github.com>
2022-08-21 07:55:52 +0700

    Update and rename dangtrinh.py to F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -69,7 +69,7 @@ class Home:
     {Color.LC}╠╣{Color.LB}{Color.LR}───{Color.LB}║ ║ ║║ ║║      {Color.LG}║ {Color.LR}Updated: {Color.LY}8/3/22      {Color.LG}║
     {Color.LC}╚{Color.LB}    ╩ ╚═╝╚═╝╩═╝{Color.LG}v2  {Color.LG}║ {Color.LB}Simple but mighty XD {Color.LG}║
                         {Color.LG}╚══════════════════════╝
-    {Color.LR}[{Color.LG}>     Made with ☕ By NguyenDangTrinh    {Color.LG}<{Color.LR}]""")
+    {Color.LR}[{Color.LG}>     Made with ☕ By FDc0d3 & Aya    {Color.LG}<{Color.LR}]""")
 		print(Color.LC+"    Type "+Color.LB+"'HELP'"+Color.LC+" to see all commands\n\n")
 		print(Color.LR+"["+Color.LG+"01"+Color.LR+"]"+Color.LC+" Proxy")
 		print(Color.LR+"["+Color.LG+"02"+Color.LR+"]"+Color.LC+" WebTool")
@@ -103,7 +103,7 @@ class Home:
 				elif option in ['dev', 'DEV']:
 					print(self.dev)
 				elif option in ['exit', 'EXIT']:
-					subprocess.run(['pkill -f dangtrinh.py'], shell=True)
+					subprocess.run(['pkill -f F-Tool.py'], shell=True)
 				elif option in ['stop', 'STOP']:
 					subprocess.run(['pkill screen'], shell=True)
 					print(f"{Color.LG} [!] Attack Stopped!")
@@ -306,7 +306,7 @@ class Tool:
 				elif option in ['dev', 'DEV']:
 					print(self.dev)
 				elif option in ['exit', 'EXIT']:
-					subprocess.run(['pkill -f dangtrinh.py'], shell=True)
+					subprocess.run(['pkill -f F-Tool.py'], shell=True)
 				elif option in ['stop', 'STOP']:
 					subprocess.run(['pkill screen'], shell=True)
 					print(f"{Color.LG} [!] Attack Stopped!")
@@ -387,7 +387,7 @@ class Tool:
 			elif option in ['dev', 'DEV']:
 				print(self.dev)
 			elif option in ['exit', 'EXIT']:
-				subprocess.run(['pkill -f dangtrinh.py'], shell=True)
+				subprocess.run(['pkill -f F-Tool.py'], shell=True)
 			elif option in ['stop', 'STOP']:
 				subprocess.run(['pkill screen'], shell=True)
 				print(f"{Color.LG} [!] Attack Stopped!")
@@ -466,7 +466,7 @@ class Tool:
 			elif option in ['dev', 'Dev']:
 				print(self.dev)
 			elif option in ['exit', 'EXIT']:
-				subprocess.run(['pkill -f dangtrinh.py'], shell=True)
+				subprocess.run(['pkill -f F-Tool.py'], shell=True)
 			elif option in ['stop', 'STOP']:
 				subprocess.run(['pkill screen'], shell=True)
 				print(f"{Color.LG} [!] Attack Stopped!")
@@ -559,7 +559,7 @@ class Tool:
 			elif option in ['dev', 'DEV']:
 				print(self.dev)
 			elif option in ['exit', 'EXIT']:
-				subprocess.run(['pkill -f dangtrinh.py'], shell=True)
+				subprocess.run(['pkill -f F-Tool.py'], shell=True)
 			elif option in ['stop', 'STOP']:
 				subprocess.run(['pkill screen'], shell=True)
 				print(f"{Color.LG} [!] Attack Stopped!")
@@ -636,7 +636,7 @@ class Tool:
 			elif option in ['dev', 'DEV']:
 				print(self.dev)
 			elif option in ['exit', 'EXIT']:
-				subprocess.run(['pkill -f dangtrinh.py'], shell=True)
+				subprocess.run(['pkill -f F-Tool.py'], shell=True)
 			elif option in ['stop', 'STOP']:
 				subprocess.run(['pkill screen'], shell=True)
 				print(f"{Color.LG} [!] Attack Stopped!")
@@ -686,7 +686,7 @@ def main():
 	for pkg in pkgs:
 		ur_mom = which(pkg)
 		if ur_mom == None:
-			dangtrinh.styleText(f"[!] {pkg} is not installed!\n")
+			F_Tool.styleText(f"[!] {pkg} is not installed!\n")
 			install = False
 		else:
 			pass
@@ -699,11 +699,11 @@ def main():
 	except IsADirectoryError:pass
 	except FileNotFoundError:
 		print(f"{Color.LR}[CRITICAL ERROR]:{Color.RESET} File: 'utils' NotFound")
-		print("\n[+] Please download on GitHub, or git clone: https://github.com/ngdangtr/dangtrinh.git\n")
+		print("\n[+] Please download on GitHub, or git clone: https://github.com/FDc0d3/F-Tool.git\n")
 		os.remove(f'{__file__}')
 		script = False
 	if script == False:sys.exit()
-	else:dangtrinh.home()
+	else:F_Tool.home()
 
 
 if __name__ == '__main__':
@@ -714,9 +714,9 @@ if __name__ == '__main__':
 {Color.LC}BBOS{Color.LR} ~> {Color.LY}L4/L7 DDOS Attack
 {Color.LC}STOP{Color.LR} ~> {Color.LY}Stop your Attack
 {Color.LC}DEV{Color.LR} ~> {Color.LY}Contact/Support dev"""
-	dev = f"""{Color.LC}Telegram{Color.LR}: {Color.LY}https://t.me/ngdangtr
-{Color.LC}Momo{Color.LR}: {Color.LY}0965849504"""
-	dangtrinh = Home(commands, dev)
+	dev = f"""{Color.LC}Telegram{Color.LR}: {Color.LY}https://t.me/FDc0d3
+{Color.LC}New[BTC]Address{Color.LR}: {Color.LY}32FGCnt4uwkkByWuH8V4qyCSfynm1iVsmB"""
+	F_Tool = Home(commands, dev)
 	Tool = Tool(commands, dev, spoof_useragents())
-	try:open('dangtrinh.py');main()
+	try:open('F-Tool.py');main()
 	except:quit()
