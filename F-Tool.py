ccadf344d15345d747b5e57482d6a8df7737bc15
Nguyễn Đăng Trình <110520177+ngdangtr@users.noreply.github.com>
2022-10-24 00:11:49 +0700

    Update F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -61,7 +61,7 @@ class Home:
 
 	def home(self): # don't edit this banner lol
 		print(f"""
-    {Color.LG}╔══════════════════════╗
+    			{Color.LG}╔══════════════════════╗
     {Color.LC}╔═╗{Color.LB} ╔╦╗╔═╗╔═╗╦      {Color.LG}║ {Color.LR}Created: {Color.LY}5/3/22      {Color.LG}║
     {Color.LC}╠╣{Color.LB}{Color.LR}───{Color.LB}║ ║ ║║ ║║      {Color.LG}║ {Color.LR}Updated: {Color.LY}8/3/22      {Color.LG}║
     {Color.LC}╚{Color.LB}    ╩ ╚═╝╚═╝╩═╝{Color.LG}v2  {Color.LG}║ {Color.LB}Simple but mighty XD {Color.LG}║
